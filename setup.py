13fc56d5c302bbfc0d6db9488f1a7590974e7673
trmttm <96355189+trmttm@users.noreply.github.com>
2022-03-20 10:09:46 +0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Utilities',
+    version='',
+    packages=['Utilities', 'Utilities.Graphs', 'Utilities.Memento'],
+    url='',
+    license='',
+    author='STTM',
+    author_email='',
+    description=''
+)
