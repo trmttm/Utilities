d20fe2ed358c62caac999766cbf13b3e1c078aeb
trmttm <96355189+trmttm@users.noreply.github.com>
2022-08-28 09:37:57 +0400

    Implement RoundUP and RoundDown

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 import os
 import shutil
 import subprocess
@@ -323,3 +324,33 @@ def copy_to_clipboard(text: str):
         subprocess.run("pbcopy", universal_newlines=True, input=text)
     except FileNotFoundError:
         subprocess.run("pbcopy", universal_newlines=True, input=text, shell=True)
+
+
+def round_decimals_up(number: float, decimals: int = 2):
+    """
+    Returns a value rounded up to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.ceil(number)
+
+    factor = 10 ** decimals
+    return math.ceil(number * factor) / factor
+
+
+def round_decimals_down(number: float, decimals: int = 2):
+    """
+    Returns a value rounded down to a specific number of decimal places.
+    """
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.floor(number)
+
+    factor = 10 ** decimals
+    return math.floor(number * factor) / factor
